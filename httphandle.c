2ac72e39e6c42cb3f68f9fdf1ad9edf3f94b785f
4396NeverBeSlaves <1398644012@qq.com>
2020-04-23 02:04:21 +0800

    add part of error handling
    and http persistent connection

--- httphandle.c
+++ httphandle.c
@@ -29,7 +29,8 @@ int accept_clients(int epfd, int lfd, httphandle* handles)
         cli_count++;
 #ifdef _DEBUG
         char client_ip[INET6_ADDRSTRLEN];
-        printf("Connection from %s:%d cfd:%d count:%d\n", inet_ntop(AF_INET6, &cli_sock.sin6_addr, client_ip, INET6_ADDRSTRLEN), ntohs(cli_sock.sin6_port),cfd,cli_count);
+        printf("Connection from %s:%d cfd:%d count:%d\n", inet_ntop(AF_INET6, &cli_sock.sin6_addr, client_ip, INET6_ADDRSTRLEN), ntohs(cli_sock.sin6_port), cfd, cli_count);
+        fflush(stdout);
 #endif
         addfd(epfd, cfd);
         handles[cfd].sock = cli_sock;
@@ -55,6 +56,7 @@ void disconnect(int epfd, httphandle* handle)
 #ifdef _DEBUG
     char client_ip[INET6_ADDRSTRLEN];
     printf("%s:%d is closing!\n", inet_ntop(AF_INET6, &handle->sock.sin6_addr, client_ip, INET6_ADDRSTRLEN), ntohs(handle->sock.sin6_port));
+    fflush(stdout);
 #endif
 
     delfd(epfd, handle->fd);
@@ -64,8 +66,7 @@ void disconnect(int epfd, httphandle* handle)
 
         // free(handle->write_buf);
 
-
-        handle->write_buf=NULL;
+        handle->write_buf = NULL;
     }
 
 #ifdef _DEBUG
@@ -82,48 +83,75 @@ int do_read(int cfd, httphandle* handle)
     char method[10], file_path[LINE_BUF_SIZE], *query_string = NULL, protocol[20];
     file_path[0] = '.';
 
+    handle->read_ptr = handle->read_buf;
+    handle->write_ptr = NULL;
     /*if ((read_count = Read(handle->fd, handle->read_buf, READ_BUF_SIZE)) == 0)
         return NEED_DISCONNECT;
     //else if (read_count == READ_BUF_SIZE)
         // send_error("HTTP Error 414. The request URL is too long");
         //return NEED_DISCONNECT;*/
+#ifdef _DEBUG
+        char client_ip[INET6_ADDRSTRLEN];
+        printf("This is %s:%d cfd:%d \n", inet_ntop(AF_INET6, &handle->sock.sin6_addr, client_ip, INET6_ADDRSTRLEN), ntohs(handle->sock.sin6_port), cfd);
+        fflush(stdout);
+#endif
 
     while (1) {
         if ((n = recv(handle->fd, handle->read_buf + read_count, READ_BUF_SIZE - read_count, 0)) <= 0) {
-            if (n == 0) {
+            if (n == 0 ) {
+                // if(handle->connection==CONNECTION_CLOSE){
 #ifdef _DEBUG
-                printf("n==0 NEED_DISCONNECT!\n");
+                    printf("n==0(recv zero byte) && handle->connection==CONNECTION_CLOSE, NEED_DISCONNECT!\n");
+                    fflush(stdout);
 #endif
-                return NEED_DISCONNECT;
+                    return NEED_DISCONNECT;
+                // }
+//                 else{
+// #ifdef _DEBUG///////////////////////////////////////////////////////////////////////
+//                     printf("n==0(recv zero byte) && handle->connection==CONNECTION_KEEP_ALIVE, NEED_READ!\n");
+//                     fflush(stdout);
+// #endif
+//                     return NEED_READ;
+//                 }
+                
+
             } else if (errno == EINTR)
                 continue;
             else if (errno == EAGAIN) {
 #ifdef _DEBUG
                 // write(STDOUT_FILENO,handle->read_buf,read_count);
-                printf("errno==EAGAIN break;\n");
+                printf("errno==EAGAIN recv end! break; \n");
+                fflush(stdout);
 #endif
                 break;
             } else
-                perror_exit("read in do_read() error!");
+                perror_exit("recv in do_read() error!");
         }
         read_count += n;
+#ifdef _DEBUG
+                handle->read_buf[read_count]='\0';
+                printf("read_count:%d content:\n%s",read_count,handle->read_buf);
+                // write(STDOUT_FILENO,handle->read_buf,read_count);
+                
+                fflush(stdout);
+#endif
     }
 
     line_size = read_line(handle, line_buf);
     if (line_size == -1) {
-// send_error("400 Bad Request");
 #ifdef _DEBUG
         printf("line_size == -1 NEED_DISCONNECT!\n");
+        fflush(stdout);
 #endif
-        return NEED_DISCONNECT;
+        return send_error_page(handle, 400, RESPONSE_STATUS_400_BAD_REQUEST);
     }
 
     if (parse_request_line(line_buf, line_size, method, &file_path[1], &query_string, protocol) == -1) {
-// send_error("400 Bad Request");
 #ifdef _DEBUG
         printf("parse_request_line() == -1 NEED_DISCONNECT!\n");
+        fflush(stdout);
 #endif
-        return NEED_DISCONNECT;
+        return send_error_page(handle, 400, RESPONSE_STATUS_400_BAD_REQUEST);
     }
 
 #ifdef _DEBUG
@@ -136,29 +164,36 @@ int do_read(int cfd, httphandle* handle)
 
     parse_request_headers(handle);
 
+    //判断是不是根目录，若是的话将返回默认首页文件
     if (strcasecmp(file_path, "./") == 0) {
         strcpy(file_path, default_index_file);
     }
 
     if (stat(file_path, &file_status) < 0) {
-        // send_error("404 not found");
-
-        return NEED_DISCONNECT;
+#ifdef _DEBUG
+        printf("file not found:%s NEED_DISCONNECT!\n", file_path);
+        fflush(stdout);
+#endif
+        return send_error_page(handle, 404, RESPONSE_STATUS_404_NOT_FOUND);
     }
     handle->send_file_size = file_status.st_size;
     if (S_ISDIR(file_status.st_mode)) {
-// send_error("403 forbidden");
+        // send_error("403 forbidden");
 #ifdef _DEBUG
         printf("S_ISDIR() NEED_DISCONNECT!\n");
+        fflush(stdout);
 #endif
-        return NEED_DISCONNECT;
+        return send_error_page(handle, 403, RESPONSE_STATUS_403_Forbidden);
     }
+
 #ifdef _DEBUG
-    printf("file_path:%s querying file_size:%ld\n", file_path, file_status.st_size);
+    printf("file_path:%s request file_size:%ld\n", file_path, file_status.st_size);
     fflush(stdout);
 #endif
+
+    //判断请求方法
     if (strcasecmp(method, "GET") == 0) {
-        //static html doc
+        //若没有查询字符串，即是静态文档
         if (query_string == NULL) {
             mount_static_doc(handle, file_path);
         } else { //dynamic doc
@@ -168,12 +203,11 @@ int do_read(int cfd, httphandle* handle)
         //
         ;
     } else {
-        // send_error(" 501 Not Implemented");
-        return NEED_DISCONNECT;
+        return send_error_page(handle, 501, RESPONSE_STATUS_501_NOT_IMPLEMENTED);
     }
 
-    send_response_headers(handle, file_path);
-    //处理完之前的任务后，当前发送缓冲区为空，开始向客户端回送数据。根据发送的结果来决定是否要继续发送，如当前发送缓冲区已满可稍后在发送
+    send_response_headers(handle, file_path, 200, RESPONSE_STATUS_200_OK);
+    //处理完之前的任务后，当前socket发送缓冲区为空，开始向客户端回送数据。根据发送的结果来决定是否要继续发送，如当前发送缓冲区已满可稍后在发
     return do_write(cfd, handle);
 }
 int do_write(int cfd, httphandle* handle)
@@ -304,8 +338,12 @@ void parse_request_headers(httphandle* handle)
         // 11 length of "Connection:"
         if (strncasecmp(line_buf, "Connection:", 11) == 0) {
             sscanf(line_buf, "%*s %s", connection_parameter);
-            if (strcasecmp(connection_parameter, "Keep-alive\r\n") == 0) {
+            if (strncasecmp(connection_parameter, "Keep-alive",11)==0) {
                 handle->connection = CONNECTION_KEEP_ALIVE;
+#ifdef _DEBUG
+                printf("set handle->connection -> CONNECTION_KEEP_ALIVE\n");
+                fflush(stdout);
+#endif
             }
         }
 
@@ -367,22 +405,22 @@ void get_content_type(char* file_path, char* content_type)
 #endif
 }
 
-void send_response_headers(httphandle* handle, char* file_path)
+void send_response_headers(httphandle* handle, char* file_path, int response_status_code, char* response_status_string)
 {
     char response_headers[1024], time_string[40], content_type[30], connetcion[20] = "Keep-alive";
-    int count = 0;
+    int count = 0, sent_count = 0, n;
     time_t t1;
     struct tm* t2;
 
     get_content_type(file_path, content_type);
-    if (handle->connection == CONNECTION_CLOSE)
+    if (handle->connection == CONNECTION_CLOSE || response_status_code != 200)
         strcpy(connetcion, "Close");
 
     t1 = time(NULL);
     t2 = gmtime(&t1);
     strftime(time_string, 40, "%a, %d %b %Y %X GMT", t2);
 
-    count += sprintf(response_headers, "HTTP/1.1 200 OK\r\n");
+    count += sprintf(response_headers, "HTTP/1.1 %d %s\r\n", response_status_code, response_status_string);
     count += sprintf(response_headers + count, "Server: X-server\r\n");
     count += sprintf(response_headers + count, "Date: %s\r\n", time_string);
     count += sprintf(response_headers + count, "Content-Length: %ld\r\n", handle->send_file_size);
@@ -397,7 +435,30 @@ void send_response_headers(httphandle* handle, char* file_path)
     fflush(stdout);
 #endif
     // Write(handle->fd, response_headers, count);
-    send(handle->fd, response_headers, count, 0);
+
+    while (1) {
+        if ((n = send(handle->fd, response_headers, count, 0)) < 0) {
+#ifdef _DEBUG
+            printf("sent_count<0!\n");
+            fflush(stdout);
+#endif
+            if (errno == EINTR || errno == EAGAIN) {
+#ifdef _DEBUG
+                printf("forcedly send! errno == EINTR || errno == EAGAIN in send_response_header()\n");
+                fflush(stdout);
+#endif
+                continue;
+            }
+        }
+        sent_count += n;
+        if (sent_count == count) {
+#ifdef _DEBUG
+            printf("All headers have sent!\n");
+            fflush(stdout);
+#endif
+            break;
+        }
+    }
 }
 
 void mount_static_doc(httphandle* handle, char* file_path)
@@ -407,16 +468,47 @@ void mount_static_doc(httphandle* handle, char* file_path)
     handle->is_static = STATIC_FILE;
     fd = open(file_path, O_RDONLY, 0);
 
-
-
     handle->write_buf = Mmap(0, handle->send_file_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
     // handle->write_buf=(char*)malloc(handle->send_file_size);
     // Read(fd,handle->write_buf,handle->send_file_size);
 
-
-
-
     handle->write_ptr = handle->write_buf;
     Close(fd);
 }
+
+int send_error_page(httphandle* handle, int response_status_code, char* response_status_string)
+{
+    char error_page_file_name[15];
+    struct stat file_stat;
+
+    sprintf(error_page_file_name, "./%d.html", response_status_code);
+    if (stat(error_page_file_name, &file_stat) < 0) {
+        char error_page[200];
+        int sent_count = 0, n;
+
+        // generate_error_page
+        sprintf(error_page, "<html><head><title>%d %s</title></head><body><h1>%d %s</h1><hr><em>Powered by X-server.</em></body></html>", response_status_code, response_status_string, response_status_code, response_status_string);
+        handle->send_file_size = strlen(error_page);
+        send_response_headers(handle, error_page_file_name, response_status_code, response_status_string);
+        while (sent_count < handle->send_file_size) {
+            if ((n = send(handle->fd, error_page + sent_count, handle->send_file_size - sent_count, 0)) < 0) {
+                if (errno == EINTR || errno == EAGAIN) {
+#ifdef _DEBUG
+                    printf("forcedly send! errno == EINTR || errno == EAGAIN in send_error_page()\n");
+                    fflush(stdout);
+#endif
+                    continue;
+                }
+            }
+            sent_count += n;
+        }
+        return NEED_DISCONNECT;
+    } else {
+        handle->send_file_size = file_stat.st_size;
+        mount_static_doc(handle, error_page_file_name);
+        send_response_headers(handle, error_page_file_name, response_status_code, response_status_string);
+        return do_write(handle->fd, handle);
+    }
+}
+
--- server.c
+++ server.c
@@ -1,3 +1,6 @@
+#include "epoll_operation.h"
+#include "httphandle.h"
+#include "wrap.h"
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <errno.h>
@@ -8,101 +11,106 @@
 #include <sys/epoll.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include "httphandle.h"
-#include "epoll_operation.h"
-#include "wrap.h"
 
-
-char *default_index_file="./index.html";
+char* default_index_file = "./index.html";
 
 int main(int argc, char** argv)
 {
-    int epfd, current_fd,lfd, events_ready, action_code;
-    int i,request_count=0;
+    int epfd, current_fd, lfd, events_ready, action_code;
+    int i, request_count = 0;
     struct epoll_event evts[MAXEVENTS];
     httphandle handles[MAXEVENTS];
 
-    int port=80;
-    char *dir="./";
+    int port = 80;
+    char* dir = "./";
 
-    if (argc == 3){
-        port=atoi(argv[1]);
-        dir=argv[2];
-    }else
+    if (argc == 3) {
+        port = atoi(argv[1]);
+        dir = argv[2];
+    } else
         printf("use: ./server <port> <www-root>\n");
-    
-    printf("running at: port:%d dir: %s\n",port,dir);
+
+    printf("running at: port:%d dir: %s\n", port, dir);
     chdir(dir);
     epfd = Epoll_create(100);
     lfd = init_listen_fd(port);
     // printf("lfd:%d\n",lfd);
     addfd(epfd, lfd);
 
-    int epoll_wait_times=0;
+    int epoll_wait_times = 0;
     while (1) {
-        #ifdef _DEBUG
-        printf("epoll wait times:%d\n",++epoll_wait_times);
+#ifdef _DEBUG
+        printf("epoll wait times:%d\n", ++epoll_wait_times);
         fflush(stdout);
-        #endif
+#endif
         events_ready = Epoll_wait(epfd, evts, MAXEVENTS, -1);
-        #ifdef _DEBUG
-        printf("events_ready:%d\n",events_ready);
+#ifdef _DEBUG
+        printf("events_ready:%d\n", events_ready);
         fflush(stdout);
-        #endif
+#endif
         // if (events_ready == 0)
         //     continue;
 
         for (i = 0; i < events_ready; ++i) {
 
-            current_fd=evts[i].data.fd;
+            current_fd = evts[i].data.fd;
 
             if (current_fd == lfd) {
-                #ifdef _DEBUG
+#ifdef _DEBUG
                 printf("client is coming!\n");
                 fflush(stdout);
-                #endif
+#endif
                 int cli_count;
-                cli_count=accept_clients(epfd, lfd, handles);
-                #ifdef _DEBUG
-                printf("current loop:No.%d  client count:%d\n\n\n\n\n\n",i+1,cli_count);
+                cli_count = accept_clients(epfd, lfd, handles);
+#ifdef _DEBUG
+                printf("current loop:No.%d  client count:%d\n\n\n\n", i + 1, cli_count);
                 fflush(stdout);
-                #endif
+#endif
 
             } else if (evts[i].events & EPOLLIN) {
-                #ifdef _DEBUG
-                printf("EPOLLIN current loop:No.%d cfd:%d request count:%d\n",i+1,current_fd,++request_count);
+#ifdef _DEBUG
+                printf("EPOLLIN current loop:No.%d cfd:%d request count:%d\n", i + 1, current_fd, ++request_count);
                 fflush(stdout);
-                #endif
-                action_code = do_read(current_fd,&handles[current_fd]);
-                // if (action_code == NEED_WRITE) {
-                //     #ifdef _DEBUG
-                //     printf("NEED_WRITE NEED_WRITE NEED_WRITE\n");
-                //     fflush(stdout);
-                //     #endif
-                //     modfd(epfd, current_fd, EPOLLOUT);
-                // } else {
-                    disconnect(epfd,&handles[current_fd]);
-                // }
+#endif
+                action_code = do_read(current_fd, &handles[current_fd]);
+                if (action_code == NEED_READ) {
+#ifdef _DEBUG
+                    printf("\n\n\n\naction_code == NEED_READ in evts[i].events & EPOLLIN\n\n\n");
+                    fflush(stdout);
+#endif
+                    // modfd(epfd, current_fd, EPOLLIN);
+                } else if (action_code == NEED_WRITE) {
+#ifdef _DEBUG
+                    printf("action_code == NEED_WRITE in evts[i].events & EPOLLIN\n");
+                    fflush(stdout);
+#endif
+                    modfd(epfd, current_fd, EPOLLOUT);
+                } else {
+                    disconnect(epfd, &handles[current_fd]);
+                }
 
-            } 
-            // else if (evts[i].events & EPOLLOUT) {
-            //     #ifdef _DEBUG
-            //     printf("EPOLLOUT current loop:No.%d cfd:%d\n",i,current_fd);
-            //     #endif
-            //     action_code = do_write(current_fd,&handles[current_fd]);
-            //     if (action_code == NEED_READ) {
-            //         modfd(epfd, current_fd, EPOLLIN);
-            //     }
-            //     // else if(action_code == NEED_WRITE){
-            //     //     modfd(epfd, current_fd, EPOLLOUT);
-            //     // }
-            //     else {
-            //         disconnect(epfd,&handles[current_fd]);
-            //     }
-                
-            // }
+            } else if (evts[i].events & EPOLLOUT) {
+#ifdef _DEBUG
+                printf("EPOLLOUT current loop:No.%d cfd:%d\n", i, current_fd);
+#endif
+                action_code = do_write(current_fd, &handles[current_fd]);
+                if (action_code == NEED_READ) {
+                    modfd(epfd, current_fd, EPOLLIN);
+#ifdef _DEBUG
+                    printf("\n\n\n\n\n\naction_code == NEED_READ in evts[i].events & EPOLLIN\n");
+                    fflush(stdout);
+#endif
+                } else if (action_code == NEED_WRITE) {
+                    modfd(epfd, current_fd, EPOLLOUT);
+#ifdef _DEBUG
+                    printf("action_code == NEED_WRITE in evts[i].events & EPOLLIN\n");
+                    fflush(stdout);
+#endif
+                } else {
+                    disconnect(epfd, &handles[current_fd]);
+                }
+            }
         }
     }
 
